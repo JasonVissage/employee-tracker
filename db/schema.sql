5a6e7f6fcc5d46d24d4da097afcba832839f03d5
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-02-04 11:39:46 -0600

    add to deeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,23 +3,25 @@ CREATE DATABASE employee_list;
 
 USE employee_list;
 
--- creates employee table
-CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    role_id INTEGER
-);
-
 -- creates dept table
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(20) NOT NULL
+    name VARCHAR(30) NOT NULL
 );
 
 -- creates role table
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(20) NOT NULL,
-    salary INTEGER(6) NOT NULL
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(6) NOT NULL,
+    dept_id INTEGER
 );
+
+-- creates employee table
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department 
+VALUES
+(1, 'HR'),
+(2, 'Customer Service'),
+(3, 'Sales'),
+(4,'IT');
+
+
+INSERT INTO role
+VALUES
+(1, 65000, 'HR Manager'),
+(2, 42000, 'Customer Service Rep'),
+(3, 80000, 'Sales Rep'),
+(4, 115000, 'Software Engineer');
+
+INSERT INTO employee
+VALUES
+(),
+(),
+(),
+();
